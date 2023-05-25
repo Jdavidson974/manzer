2148fc6cb21a95303ab20a64c6e385d2ed67641a
Jdavidson <jd9743101@gmail.com>
2023-05-25 15:27:34 +0400

    ajout de l'interceptor pour les requette + requete get profil + model associé au profil

--- src/app/layout/components/layout/layout.component.ts
+++ src/app/layout/components/layout/layout.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Profil } from 'src/app/auth/models/profil.model';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { DataState } from 'src/app/shared/state/datastate';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.sass']
 })
-export class LayoutComponent {
-  urlServer: string = "http://localhost:3000/auth/login-google"
+export class LayoutComponent implements OnInit {
+  constructor(private authService: AuthService) { }
+  urlServer: string = "http://localhost:3000/auth/login-google";
+  profil !: DataState<Profil>;
+  ngOnInit(): void {
+
+  }
+
 }
