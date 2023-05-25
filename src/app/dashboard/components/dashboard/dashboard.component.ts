cf3638752d043a63631c82dc93dacf0557cd21f2
Jdavidson <jd9743101@gmail.com>
2023-05-25 06:52:30 +0400

    page dashboard

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent {
-
+  iconSearch = faSearch;
 }
