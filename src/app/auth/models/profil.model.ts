565a957ad14c7f384eece0133401160e51e288d1
Jdavidson <jd9743101@gmail.com>
2023-05-26 14:23:42 +0400

    filter ok

--- src/app/auth/models/profil.model.ts
+++ src/app/auth/models/profil.model.ts
@@ -1,6 +1,9 @@
+import { Secteur } from "src/app/home/models/secteur.model";
+
 export class Profil {
     id!: number;
     username!: string;
     email!: string;
     picture!: string;
+    secteur !: Secteur
 }
--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -6,6 +6,8 @@ import { Secteur } from '../../models/secteur.model';
 import { SecteurService } from '../../services/secteur.service';
 import { RepasService } from '../../services/repas.service';
 import { Repas } from '../../models/repas.model';
+import { Observable, of, pipe, tap } from 'rxjs';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -19,18 +21,113 @@ export class HomeComponent implements OnInit {
     private router: Router,
     private repasService: RepasService,
     private secteurService: SecteurService,
+    private formBuilder: FormBuilder,
   ) { }
   iconSearch = faSearch;
   secteurs !: DataState<Secteur[]>;
+  secteurs$ !: Observable<Secteur[] | null>;
   repas !: DataState<Repas[]>;
+  repasTab !: Observable<Repas[] | null>;
+  initRepasTab !: Observable<Repas[] | null>;
+  formSecteur!: FormGroup;
   ngOnInit(): void {
+    this.formSecteur = this.formBuilder.group({
+      checkbox: this.formBuilder.array([],)
+    })
+    const formArray = this.formSecteur.get('checkbox') as FormArray;
     this.secteurs = this.secteurService.secteursState;
+    this.secteurs$ = this.secteurs.value$.pipe(
+      tap(
+        secteurs => {
+          if (secteurs) {
+            secteurs.map(
+              secteur => {
+                formArray.push(this.formBuilder.group({
+                  checked: this.formBuilder.control(false),
+                  value: this.formBuilder.control(secteur.id),
+                }))
+              }
+            );
+            //EXECUTER LE CODE POUR LE FILTRE ICI 
+            this.formSecteur.valueChanges.pipe(
+              tap(
+                (value: { checkbox: { checked: boolean, value: number }[] }) => {
+                  console.log(value
+                  );
+
+                  //je check tout les box checked,
+                  // je met toute les valeur dans un tableau 
+                  const tabId: number[] = [];
+                  value.checkbox.forEach(item => {
+                    if (item.checked) {
+                      tabId.push(item.value)
+                    }
+                  })
+
+                  // je verifie si le tableau est pas vide 
+                  //Si vide repas reprend sa valuer initial
+                  //si pas vide chercher tout les repas qui ont les id stocker dans le tableau
+                  // repas prend la valeur du tableau en observable
+                  if (tabId.length) {
+                    this.initRepasTab.pipe(
+                      tap(
+                        repas => {
+                          if (repas?.length) {
+                            let newRepasTab: Repas[] = [];
+                            tabId.forEach(id => {
+                              const item = repas.filter(item => item.user.secteur.id == id);
+                              if (item) {
+                                newRepasTab = newRepasTab.concat(item)
+                              }
+                            });
+                            this.repasTab = of(newRepasTab);
+                          }
+                        }
+                      )
+                    ).subscribe()
+                  } else {
+                    this.repasTab = this.initRepasTab;
+                  }
+
+
+                }
+              )
+            ).subscribe();
+
+          }
+        }
+      )
+    );
     this.repas = this.repasService.repasState;
+
+    this.repasTab = this.repas.value$.pipe(
+      tap(
+        value => {
+          this.initRepasTab = of(value);
+        }
+      )
+    );
+
     const token = this.route.snapshot.queryParamMap.get('token');
     if (token) {
       localStorage.setItem('token', token);
       this.router.navigateByUrl('/mes-repas')
     }
+
   }
 
+  filterSecteur(id: number) {
+
+
+  }
+
+  // this.initRepasTab.pipe(
+  //   tap(
+  //     repas => {
+  //       repas.fi
+  //     }
+  //   )
+  // ).subscribe()
+
 }
+
